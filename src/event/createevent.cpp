9d3e6cb6ac7759e82a142ceb7afdc4dd75e05049
Sven Starcke <sst97@gmx.net>
2019-10-19 17:26:46 +0200

    added stringification for capnzero Protocol

--- src/event/createevent.cpp
+++ src/event/createevent.cpp
@@ -15,11 +15,28 @@ CreateEvent::CreateEvent()
 
 const std::string CreateEvent::toYaml() const
 {
+  std::string protocolString;
+
+  switch(protocol)
+  {
+    case capnzero::Protocol::UDP:
+      protocolString = "UDP";
+      break;
+    case capnzero::Protocol::TCP:
+      protocolString = "TCP";
+      break;
+    case capnzero::Protocol::IPC:
+      protocolString = "IPC";
+      break;
+    default:
+      protocolString = "undefined";
+  }
+
   YAML::Emitter yaml;
   yaml << YAML::BeginMap
-       << YAML::Key << "id" << YAML::Value << id
+       << YAML::Key << "id" << YAML::Value <<  id
        << YAML::Key << "type" << YAML::Value << type
-       << YAML::Key << "protocol" << YAML::Value << protocol
+       << YAML::Key << "protocol" << YAML::Value << protocolString
        << YAML::EndMap;
 
   return yaml.c_str();
